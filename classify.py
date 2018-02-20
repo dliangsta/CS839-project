447ec45ade55a2c3f66c81ea3a06d193d59b4fda
Viswesh Periyasamy <visweshrp@gmail.com>
2018-02-20 12:48:35 -0600

    classifier script outline

---
+++ classify.py
@@ -0,0 +1,28 @@
+import json
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LinearRegression, LogisticRegression
+import numpy as np
+
+def main():
+    
+
+def decision_tree(X, y):
+    clf = DecisionTreeClassifier(random_state=0)
+
+def random_forest(X, y):
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+
+def svm(X, y):
+    clf = SVC()
+
+def linear_regression(X, y):
+    lr = LinearRegression()
+
+def logistic_regression(X, y):
+    lr = LogisticRegression()
+        
+if __name__ == '__main__':
+    main()
+    

2d11f68d6411d5cb6dc7fb536fcfe6921f5fbc6f
David Liang <davidliangx27@gmail.com>
2018-02-21 16:48:01 -0600

    added pruning based on list

--- vectorize.py
+++ vectorize.py
@@ -1,14 +1,15 @@
 import pickle
+import json
 from instance import *
 from location import *
-
-
-prune_list = ["the",  "be",  "to",  "of",  "and",  "a",  "in",  "that",  "have",  "I",  "it",  "for",  "not",  "on",  "with",  "he",  "as",  "you",  "do",  "at",  "this",  "but",  "his",  "by",  "from",  "they",  "we",  "say",  "her",  "she",  "or",  "an",  "will",  "my",  "one",  "all",  "would",  "there",  "their",  "what",  "so",  "up",  "out",  "if",  "about",  "who",  "get",  "which",  "go",  "me",  "when",  "make",  "can",  "like",  "time",  "no",  "just",  "him",  "know",  "take",  "people",  "into",  "year",  "your",  "good",  "some",  "could",  "them",  "see",  "other",  "than",  "then",  "now",  "look",  "only",  "come",  "its",  "over",  "think",  "also",  "back",  "after",  "use",  "two",  "how",  "our",  "work",  "first",  "well",  "way",  "even",  "new",  "want",  "because",  "any",  "these",  "give",  "day",  "most",  "us"]
-
 
 def main():
     instances = []
     # Iterate over all documents.
+
+    with open('data/prune_list.json') as f:
+        prune_list = json.load(f)
+
     for i in range(1,331):
         with open('data/labeled/' + str(i)) as f:
             data = [line.strip() for line in f.readlines()]
@@ -19,43 +20,46 @@ def main():
 
                 # Skip every other line, because we handle the lines in pairs.
                 if j % 2 == 1: 
-                    # Iterate over each character in the line.
-                    for k in range(len(labeled_line)):
+
+                    # Iterate over each character in the line to find each word.
+                    k = 0
+                    while k < len(labeled_line):
                         label = 1 if labeled_line[k] == '1' else 0
                         # Index of end of label.
                         l = labeled_line.find(' ', k)
 
-                        label = labeled_line[k:l]
                         word = original_line[k:l]
-
-                        if not shouldPruneWord(word):
+                        if not shouldPruneWord(word, prune_list):
                             # Make features.
-                            features = []
-                            features.append(hasAllCaps(word))
-                            features.append(surroundedByParentheses(word))
-                            features.append(wordLength(word))
-                            features.append(firstLetterCapitalized(word))
-                            features.append(containsCashSubstring(word))
-                            features.append(containsCoinSubstring(word))
-                            features.append(numCapitals(word))
+                            print(word)
+                            # features = []
+                            # features.append(hasAllCaps(word))
+                            # features.append(surroundedByParentheses(word))
+                            # features.append(wordLength(word))
+                            # features.append(firstLetterCapitalized(word))
+                            # features.append(containsCashSubstring(word))
+                            # features.append(containsCoinSubstring(word))
+                            # features.append(numCapitals(word))
 
 
-                            location = Location(i, j, k)
-                            instance = Instance(location=location, 
-                                                label=label, 
-                                                word=word,
-                                                features=features)
-                            instances.append(instance)
+                            # location = Location(i, j, k)
+                            # instance = Instance(location=location, 
+                            #                     label=label, 
+                            #                     word=word,
+                            #                     features=features)
+                            # instances.append(instance)
 
-                            print(instance)
+                            # print(instance)
 
+                        k = l+1
 
     # Save dataset.
     with open('data/instances.pkl','wb') as f:
         pickle.dump(instances, f)
 
-def shouldPruneWord(word):
-    return False
+def shouldPruneWord(word, prune_list):
+    word = removePunctuation(word)
+    return word in prune_list
 
 # Features
 def hasAllCaps(word):
@@ -89,9 +93,9 @@ def numCapitals(word):
 # Utilities
 def removePunctuation(word):
     punctuation = ['\'','"','.','?','!',',',';',':']
-    while word[0] in punctuation:
+    while len(word) and word[0] in punctuation:
         word = word[1:]
-    while word[-1] in punctuation:
+    while len(word) and word[-1] in punctuation:
         word = word[:-1]
     return word
 

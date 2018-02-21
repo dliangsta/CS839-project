6bccc98100ad92dba53cd24d7a1630d291f395fb
David Liang <davidliangx27@gmail.com>
2018-02-21 14:24:39 -0600

    added 2 features: caps, parentheses

--- vectorize.py
+++ vectorize.py
@@ -10,18 +10,46 @@ def main():
             for i, labeled_line in enumerate(data):
                 original_line = data[i-1]
                 if i % 2 == 1:
-                    labeled_line_split = labeled_line.split(' ')
-                    original_line_split = original_line.split(' ')
-                    for j, label in enumerate(labeled_line_split):
-                        if label[0] == '1':
-                            print(label, original_line_split[j])
+                    for j in range(len(labeled_line)):
+                        if labeled_line[j] == '1':
+                            k = labeled_line.find(' ',j)
+                            label = labeled_line[j:k]
+                            word = original_line[j:k]
+                            print(label)
+                            print(word)
                             # Make features
+                            features = []
+                            features.append(hasAllCaps(word))
+                            features.append(surroundedByParentheses(word))
+                            print(features)
+
+
                             location = Location(i, j)
-                            instance = Instance(location, 1)
+                            instance = Instance(location=location, 
+                                                label=1, 
+                                                features=features)
                             instances.append(instance)
 
     with open('data/instances.pkl','wb') as f:
         pickle.dump(instances, f)
+
+def hasAllCaps(word):
+    word = removePunctuation(word)
+    return int(word.upper() == word)
+
+def surroundedByParentheses(word):
+    # Remove leading and trailing quotations
+    word = removePunctuation(word)
+    return int(word[0] == '(' and word[-1] == ')')
+
+def removePunctuation(word):
+    punctuation = ['\'','"','.','?','!',',',';',':']
+    while word[0] in punctuation:
+        word = word[1:]
+    while word[-1] in punctuation:
+        word = word[:-1]
+    return word
+
 
 
 if __name__ == '__main__':

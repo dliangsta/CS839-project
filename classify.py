7177017a4a9a150f2c828c89318fae3c923b5547
Viswesh Periyasamy <visweshrp@gmail.com>
2018-02-23 00:09:07 -0600

    added precision and recall

--- classify.py
+++ classify.py
@@ -4,7 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
+from sklearn.metrics import precision_score, recall_score
 from instance import *
 from location import *
 import numpy as np
@@ -27,41 +27,77 @@ def main():
     lir_acc_cv = 0
     lor_acc_cv = 0
 
+    dt_p_cv = 0
+    rf_p_cv = 0
+    svm_p_cv = 0
+    lir_p_cv = 0
+    lor_p_cv = 0
+
+    dt_r_cv = 0
+    rf_r_cv = 0
+    svm_r_cv = 0
+    lir_r_cv = 0
+    lor_r_cv = 0
+
     for k, (train_index, test_index) in enumerate(kf.split(X)):
         X_train, X_test = X[train_index], X[test_index]		
         y_train, y_test = y[train_index], y[test_index]
 		
-        dt_acc = decision_tree(X_train, X_test, y_train, y_test)		
-        rf_acc = random_forest(X_train, X_test, y_train, y_test)		
-        svm_acc = svm(X_train, X_test, y_train, y_test)		
-        lir_acc = linear_regression(X_train, X_test, y_train, y_test)		
-        lor_acc = logistic_regression(X_train, X_test, y_train, y_test)
+        dt_acc, dt_p, dt_r = decision_tree(X_train, X_test, y_train, y_test)		
+        rf_acc, rf_p, rf_r = random_forest(X_train, X_test, y_train, y_test)		
+        svm_acc, svm_p, svm_r = svm(X_train, X_test, y_train, y_test)		
+        lir_acc, lir_p, lir_r = linear_regression(X_train, X_test, y_train, y_test)		
+        lor_acc, lor_p, lor_r = logistic_regression(X_train, X_test, y_train, y_test)
 		
         print('fold %d' % k)		
-        print('decision tree accuracy: %f' % dt_acc)		
-        print('random forest accuracy: %f' % rf_acc)		
-        print('svm accuracy: %f' % svm_acc)		
-        print('linear regression accuracy: %f' % lir_acc)	
-        print('logistic regression accuracy: %f\n' % lor_acc)
+        print('decision tree accuracy: %f, precision: %f, recall: %f' % dt_acc, dt_p, dt_r)		
+        print('random forest accuracy: %f, precision: %f, recall: %f' % rf_acc, rf_p, rf_r)		
+        print('svm accuracy: %f, precision: %f, recall: %f' % svm_acc, svm_p, svm_r)		
+        print('linear regression accuracy: %f, precision: %f, recall: %f' % lir_acc, lir_p, lir_r)
+        print('logistic regression accuracy: %f, precision: %f, recall: %f' % lor_acc, lor_p, lor_r)
 		
         dt_acc_cv += dt_acc		
         rf_acc_cv += rf_acc		
         svm_acc_cv += svm_acc		
         lir_acc_cv += lir_acc		
         lor_acc_cv += lor_acc
+
+        dt_p_cv += dt_p
+        rf_p_cv += rf_p
+        svm_p_cv += svm_p
+        lir_p_cv += lir_p
+        lor_p_cv += lor_p
+
+        dt_r_cv += dt_r
+        rf_r_cv += rf_r
+        svm_r_cv += svm_r
+        lir_r_cv += lir_r
+        lor_r_cv += lor_r
 	
     dt_acc_cv /= SPLITS	
     rf_acc_cv /= SPLITS	
     svm_acc_cv /= SPLITS	
     lir_acc_cv /= SPLITS	
     lor_acc_cv /= SPLITS
+
+    dt_p_cv /= SPLITS
+    rf_p_cv /= SPLITS
+    svm_p_cv /= SPLITS
+    lir_p_cv /= SPLITS
+    lor_p_cv /= SPLITS
+
+    dt_r_cv /= SPLITS
+    rf_r_cv /= SPLITS
+    svm_r_cv /= SPLITS
+    lir_r_cv /= SPLITS
+    lor_r_cv /= SPLITS
 	
     print('Final Results')	
-    print('decision tree accuracy: %f' % dt_acc_cv)	
-    print('random forest accuracy: %f' % rf_acc_cv)	
-    print('svm accuracy: %f' % svm_acc_cv)	
-    print('linear regression accuracy: %f' % lir_acc_cv)	
-    print('logistic regression accuracy: %f\n' % lor_acc_cv)
+    print('decision tree accuracy: %f, precision: %f, recall: %f' % dt_acc_cv, dt_p_cv, dt_r_cv)     
+    print('random forest accuracy: %f, precision: %f, recall: %f' % rf_acc_cv, rf_p_cv, rf_r_cv)     
+    print('svm accuracy: %f, precision: %f, recall: %f' % svm_acc_cv, svm_p_cv, svm_r_cv)        
+    print('linear regression accuracy: %f, precision: %f, recall: %f' % lir_acc_cv, lir_p_cv, lir_r_cv)
+    print('logistic regression accuracy: %f, precision: %f, recall: %f' % lor_acc_cv, lor_p_cv, lor_r_cv)
     
     clfs = ['Decision Tree', 'Random Forest', 'SVM', 'Linear Regression', 'Logistic Regression']
     accs = [dt_acc_cv, rf_acc_cv, svm_acc_cv, lir_acc_cv, lor_acc_cv]
@@ -71,28 +107,43 @@ def main():
 def decision_tree(X_train, X_test, y_train, y_test):
     clf = DecisionTreeClassifier(random_state=0)
     clf.fit(X_train, y_train)
-    return clf.score(X_test, y_test)
+    y_predict = clf.predict(X_test)
+    precision = precision_score(y_test, y_predict)
+    recall = recall_score(y_test, y_predict)
+    return clf.score(X_test, y_test), precision, recall
 
 
 def random_forest(X_train, X_test, y_train, y_test):
     clf = RandomForestClassifier(random_state=0)
     clf.fit(X_train, y_train)
-    return clf.score(X_test, y_test)
+    y_predict = clf.predict(X_test)
+    precision = precision_score(y_test, y_predict)
+    recall = recall_score(y_test, y_predict)
+    return clf.score(X_test, y_test), precision, recall
 
 def svm(X_train, X_test, y_train, y_test):
     clf = SVC()
     clf.fit(X_train, y_train)
-    return clf.score(X_test, y_test)
+    y_predict = clf.predict(X_test)
+    precision = precision_score(y_test, y_predict)
+    recall = recall_score(y_test, y_predict)
+    return clf.score(X_test, y_test), precision, recall
 
 def linear_regression(X_train, X_test, y_train, y_test):
     lr = LinearRegression()
     lr.fit(X_train, y_train)
-    return lr.score(X_test, y_test)
+    y_predict = lr.predict(X_test)
+    precision = precision_score(y_test, y_predict)
+    recall = recall_score(y_test, y_predict)
+    return lr.score(X_test, y_test), precision, recall
 
 def logistic_regression(X_train, X_test, y_train, y_test):
     lr = LogisticRegression()
     lr.fit(X_train, y_train)
-    return lr.score(X_test, y_test)
+    y_predict = lr.predict(X_test)
+    precision = precision_score(y_test, y_predict)
+    recall = recall_score(y_test, y_predict)
+    return lr.score(X_test, y_test), precision, recall
         
 if __name__ == '__main__':
     main()

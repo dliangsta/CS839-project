3d60e04d1005e97854c123267a248e61c8811387
David Liang <davidliangx27@gmail.com>
2018-02-23 15:37:23 -0600

    updated duplicate detection script to dump remove list

--- duplicates.py
+++ duplicates.py
@@ -1,11 +1,29 @@
 import json
+from difflib import SequenceMatcher
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
 
 data = []
 for i in range(1,331):
-    with open('data/unlabeled/'+str(i)) as f:
-        data.append(json.load(f))
+    try:
+        with open('data/unlabeled/'+str(i)) as f:
+            data.append(json.load(f))
+    except:
+        pass
 
-seen = set()
-uniq = [x for x in data if x not in seen and not seen.add(x)]  
-print(len(uniq))
-print(len(data))
+S = []
+remove = []
+for i, article in enumerate(data):
+    for j, article2 in enumerate(data):
+        if j > i and j not in remove and i not in remove:
+            s = similar(article, article2)
+            if s > .5:
+                remove.append(j)
+                print(i,j,s)
+                S.append(s)
+
+with open ('data/duplicate_documents.json') as f:
+    json.dump(remove)
+    
+print(remove)   

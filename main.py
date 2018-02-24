4a7d0d92a08f735e26a8182a1d11441978ac103d
David Liang <davidliangx27@gmail.com>
2018-02-24 14:05:41 -0600

    combined files

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
+import datetime
 import json
-import re
+from dateutil.relativedelta import relativedelta
+from eventregistry import *
 
 CRYPTO = '1'
 UNSURE = '`'
-DIRECTORY = 'data/unlabeled/'
 
 def main():
+    # scrape()
+    split()
+    prepare()
+
+def scrape():
+    er = EventRegistry(apiKey = 'c4e1c037-d70b-4c75-a11e-b98809a5fc47')
+
+    q = QueryArticlesIter(keywords=QueryItems.AND(['cryptocurrency','$','price']), 
+                        lang='eng')
+                        
+    # Note, the results are not the same every time.
+    results = q.execQuery(er, sortBy = 'date', maxItems=1000)
+
+    text = []
+    for article in results:
+        # Results are not guaranteed to be unique.
+        if article['body'] not in text:
+            text.append(article['body'])
+    print(text)
+
+    with open('data/raw.json','w') as f:
+        json.dump(text, f)
+
+def split():
+    with open('data/raw.json') as f:
+        data = json.load(f)
+        print(len(data))
+    for i, article in enumerate(data):
+        if i < 330:
+            with open('data/unlabeled/' + str(i+1), 'w') as ff:
+                json.dump(article, ff)
+def prepare():
     with open('data/cryptocurrencies_list.json') as f:
         # Take top n currencies.
         n = 100
         cryptocurrencies = [cryptocurrency_name.lower() for cryptocurrency_name in json.load(f)[:n]]
 
     for i in range(1,331):
-        with open(DIRECTORY + str(i)) as f:
+        with open('data/unlabeled/' + str(i)) as f:
             article = json.load(f) 
 
 
@@ -47,8 +80,7 @@ def label(original_word, cryptocurrencies):
     word = re.split('\'', word)[0].lower()
 
     return CRYPTO if word in cryptocurrencies or word[:-1] in cryptocurrencies else UNSURE
-        
 
 if __name__ == '__main__':
     main()
-
+    
--- pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Order of execution of python scripts.
-python3.5 create_cryptocurrencies_list.py
-# python3.5 scrape.py
-python3.5 split.py
-python3.5 prepare.py

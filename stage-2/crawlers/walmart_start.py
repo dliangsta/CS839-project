51932309b3bf86ac63cada29239f2f8865ad1590
David Liang <davidliangx27@gmail.com>
2018-03-22 18:10:58 -0500

    cleanup

--- stage-2/crawlers/walmart_start.py
+++
@@ -1,6 +0,0 @@
-from helpers import make_request, log, format_url
-
-url = 'https://www.walmart.com/browse/electronics/tvs/3944_1060825_447913?povid=3944+%7C+contentZone2+%7C+2016-10-21+%7C+1+%7C+Shop_by_Cat_TVs'
-page, html = make_request(url)
-for item in page.findAll("div", "search-result-gridview-item clearfix"):
-	print(item.find('a')['href'])

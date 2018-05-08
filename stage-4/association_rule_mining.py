1c04f68f2bbe1a65659fd4504a3981d1d8b360ee
Aishwarya Ganesan <ash8aster@gmail.com>
2018-05-08 17:22:50 -0500

    association rules

--- stage-4/association_rule_mining.py
+++ stage-4/association_rule_mining.py
@@ -3,6 +3,7 @@ import mlxtend
 from collections import defaultdict
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
+import sys
 df = pd.read_csv('./data/merged_clean_numeric_v2.csv')
 
 suffixes = {}
@@ -14,7 +15,7 @@ suffixes['ram size'] = ' GB'
 #'processor (cpu) manufacturer'
 all_dict = defaultdict(list)
 for data in df:
-	if data in ['processor (cpu) manufacturer', 'brand', 'hard disk size', 'operating system', 'processor count', 'processor speed', 'ram size', 'screen size']:
+	if data in ['brand', 'hard disk size', 'operating system', 'processor count', 'processor speed', 'ram size', 'screen size']:
 		for i in range(0, len(df[data])):
 			all_dict['id'].append(i)
 			val = str(data).replace(' ', '_') + ': ' + str(df[data][i])
@@ -29,8 +30,9 @@ basket = (df
           .sum().unstack().reset_index().fillna(0)
           .set_index('id'))
 
-frequent_itemsets = apriori(basket, min_support=0.3, use_colnames=True)
+frequent_itemsets = apriori(basket, min_support=0.45, use_colnames=True)
 
+'''
 print ('Support\tLengthofSet\tFrequent_Item_Set')
 for i in range(0, len(frequent_itemsets['support'])):
 	support = frequent_itemsets['support'][i]
@@ -41,7 +43,12 @@ for i in range(0, len(frequent_itemsets['support'])):
 
 	to_print = str(round(support, 2)) + '\t' + str(len(itemsets)) + '\t[' + to_print[1:] + ' ]'
 	print (to_print)
+sys.exit(0)
+'''
 
 rules = association_rules(frequent_itemsets)
+rules = rules[(rules['confidence'] >= 0.8)]
 
-#print(rules[(rules['confidence'] >= 0.8) ])
+print ('AssociationRule\tSupport\tConfidence')
+for i in range(0, len(rules['support'])):
+	print (str(rules['antecedants'][i]).replace('frozenset', '') + ' => ' + str(rules['consequents'][i]).replace('frozenset', '') + '\t' + str(round(rules['support'][i],2)) + '\t' + str(round(rules['confidence'][i],2)))

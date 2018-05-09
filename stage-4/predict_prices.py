f99b8ad3f842c5e1124bf67b421648ce571c80ce
Viswesh Periyasamy <visweshrp@gmail.com>
2018-05-09 17:02:54 -0500

    updated price prediction script

--- stage-4/predict_prices.py
+++ stage-4/predict_prices.py
@@ -9,6 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_score, recall_score, accuracy_score
+from sklearn.model_selection import KFold
 
 labels = ['$0-$200', '$200-$400', '$400-$600', '$600-$800', '$800-$1000', '$1000-$1200',
 		   '$1200-$1400', '$1400-$1600', '$1600-$1800', '$1800-$2000', '$2000-$2200',
@@ -17,6 +18,14 @@ labels = ['$0-$200', '$200-$400', '$400-$600', '$600-$800', '$800-$1000', '$1000
 		   '$4200-$4400', '$4400-$4600', '$4600-$4800', '$4800-$5000', '$5000-$5200',
 		   '$5200-$5400', '$5400-$5600', '$5600-$5800', '$5800-$6000', '$6000-$6200',
 		   '$6200-$6400', '$6400-$6600']
+
+s_labels = ['$0-$500', '$500-$1000', '$1000-$1500', '$1500-$2000', '$2000-$2500',
+		    '$2500-$3000', '$3000-$3500', '$3500-$4000', '$4000-$4500', '$4500-$5000',
+		    '$5000-$5500', '$5500-$6000', '$6000-$6500',]
+
+increment = 500
+max_price = 6500
+boundary = max_price + increment
 
 def create_data(in_csv):
 	with open('./data/' + in_csv, 'r') as f:
@@ -103,16 +112,16 @@ def create_data(in_csv):
 	y = y.astype(np.float64)
 
 	# categorize prices into $200 quantile buckets
-	num_buckets = (6600 / 200) + 1
+	num_buckets = (max_price / increment) + 1
 
-	buckets = list(range(0, 6800, 200))
+	buckets = list(range(0, boundary, increment))
 	indices = np.digitize(y, bins=buckets, right=True)
 	y_cat = np.empty(y.shape, dtype=object)
 
 	# replace all values with categorical label strings
 	for i in range(len(y)):
 		for j in range(len(y[i])):
-			y_cat[i,j] = labels[indices[i,j]-1]
+			y_cat[i,j] = s_labels[indices[i,j]-1]
 
 
 	#print(y_cat[:10])
@@ -124,7 +133,7 @@ def predict_prices(X, y):
 
 	# convert string labels to numerical
 	le = LabelEncoder()
-	le.fit(labels)
+	le.fit(s_labels)
 	y_a = le.transform(y[:,0])
 	y_w = le.transform(y[:,1])
 	y = np.stack((y_a, y_w), axis=1)
@@ -139,50 +148,74 @@ def predict_prices(X, y):
 			  #(lr, 'Logistic Regression')]#,
 			  #(nb, 'Gaussian Naive Bayes')]
 
-	# split X,y into train and test
-	split_index = int(np.ceil((2.0/3) * len(X)))
-	
-	X_train = X[:split_index]
-	X_test = X[split_index:]
-
-	y_train = y[:split_index]
-	y_test = y[split_index:]
+	# use 5 fold cross validation
+	kf = KFold(n_splits=5)
+	kf.get_n_splits(X)
 
 	# Train models and make predictions using the testing set
 	for model in models:
-		clf = model[0]
-		clf.fit(X_train, y_train)
-		y_pred = clf.predict(X_test)
 
-		# Statistics
-		a_accuracy = accuracy_score(y_test[:,0], y_pred[:,0])
-		a_precision = precision_score(y_test[:,0], y_pred[:,0], average='weighted')
-		a_recall = recall_score(y_test[:,0], y_pred[:,0], average='weighted')
-		try:
-			a_f1 = 2*a_precision*a_recall/(a_precision+a_recall)
-		except:
-			a_f1 = -1.
+		a_accuracy_final = []
+		a_precision_final = []
+		a_recall_final = []
+		a_f1_final = []
+		w_accuracy_final = []
+		w_precision_final = []
+		w_recall_final = []
+		w_f1_final = []
 
-		w_accuracy = accuracy_score(y_test[:,1], y_pred[:,1])
-		w_precision = precision_score(y_test[:,1], y_pred[:,1], average='weighted')
-		w_recall = recall_score(y_test[:,1], y_pred[:,1], average='weighted')
-		try:
-			w_f1 = 2*w_precision*w_recall/(w_precision+w_recall)
-		except:
-			w_f1 = -1.
+		for train_index, test_index in kf.split(X):
+
+			# partition data
+			X_train, X_test = X[train_index], X[test_index]
+			y_train, y_test = y[train_index], y[test_index]
+
+			clf = model[0]
+			clf.fit(X_train, y_train)
+			y_pred = clf.predict(X_test)
+
+			# Statistics
+			a_accuracy = accuracy_score(y_test[:,0], y_pred[:,0])
+			a_precision = precision_score(y_test[:,0], y_pred[:,0], average='weighted')
+			a_recall = recall_score(y_test[:,0], y_pred[:,0], average='weighted')
+			try:
+				a_f1 = 2*a_precision*a_recall/(a_precision+a_recall)
+			except:
+				a_f1 = -1.
+
+			w_accuracy = accuracy_score(y_test[:,1], y_pred[:,1])
+			w_precision = precision_score(y_test[:,1], y_pred[:,1], average='weighted')
+			w_recall = recall_score(y_test[:,1], y_pred[:,1], average='weighted')
+			try:
+				w_f1 = 2*w_precision*w_recall/(w_precision+w_recall)
+			except:
+				w_f1 = -1.
+
+			# print results
+			print('\nClassifier: %s' % model[1])
+			print('Accuracy:\tamazon - %.3f\twalmart - %.3f' % (a_accuracy, w_accuracy))
+			print('Precision:\tamazon - %.3f\twalmart - %.3f' % (a_precision, w_precision))
+			print('Recall:\tamazon - %.3f\twalmart - %.3f' % (a_recall, w_recall))
+			print('F1:\tamazon - %.3f\twalmart - %.3f' % (a_f1, w_f1))
+
+			# save results to compute average
+			a_accuracy_final.append(a_accuracy)
+			a_precision_final.append(a_precision)
+			a_recall_final.append(a_recall)
+			a_f1_final.append(a_f1)
+
+			w_accuracy_final.append(w_accuracy)
+			w_precision_final.append(w_precision)
+			w_recall_final.append(w_recall)
+			w_f1_final.append(w_f1)
 
 		# print results
-		print('\nClassifier: %s' % model[1])
-		print('Accuracy:\tamazon - %.3f\twalmart - %.3f' % (a_accuracy, w_accuracy))
-		print('Precision:\tamazon - %.3f\twalmart - %.3f' % (a_precision, w_precision))
-		print('Recall:\tamazon - %.3f\twalmart - %.3f' % (a_recall, w_recall))
-		print('F1:\tamazon - %.3f\twalmart - %.3f' % (a_f1, w_f1))
-
-	# view predictions
-	#pred_vs_real = np.concatenate((y_test, y_pred), axis=1)
-	#print(pred_vs_real[:20])
-
-	return None
+		print('\n\n*****Final Results*****')
+		print('Classifier: %s' % model[1])
+		print('Accuracy:\tamazon - %f\twalmart - %f' % (np.mean(a_accuracy_final), np.mean(w_accuracy_final)))
+		print('Precision:\tamazon - %f\twalmart - %f' % (np.mean(a_precision_final), np.mean(w_precision)))
+		print('Recall:\tamazon - %f\twalmart - %f' % (np.mean(a_recall_final), np.mean(w_recall_final)))
+		print('F1:\tamazon - %f\twalmart - %f' % (np.mean(a_f1_final), np.mean(w_f1_final)))
 
 
 def main(args):
